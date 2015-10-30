4fd27b4e123c18975b7e96fd446de9acefda740e
steven <steven@stevendeMacBook-Air.local>
2015-10-30 23:56:11 +0800

    同上

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "frontend.h"
 #include <QApplication>
-
+#include "kernel.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     FrontEnd w;
     w.show();
-
+    Structure t(textContent("this is a sample text"));
+    t.addChildContent(textContent("this is paragraph 1"));
+    Translator::translate(t,"test.pdf");
     return a.exec();
 }
--- test.cpp
+++
@@ -1,7 +0,0 @@
-#include "kernel.h"
-int main()
-{
-    Structure a(textContent("this is a sample text"));
-    a.addChildContent(textContent("this is paragraph 1"));
-    Translator::translate(a,"test.pdf");
-}
