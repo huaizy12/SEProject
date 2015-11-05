3f6aad5857d95eaf3bba6ba7b38423706b023344
StevenChenSE <chenjw13@icloud.com>
2015-11-05 21:28:28 +0800

    增加了一个平台相关的宏

--- translator.h
+++ translator.h
@@ -10,14 +10,18 @@ class Translator
 public:
     static void translate(const Structure & root,std::string target="untitled"){
         std::ofstream boat;
-        boat.open(target);
+        boat.open(target.c_str());
         boat<<"\\documentclass{article}"<<std::endl;
         boat<<"\\begin{document}"<<std::endl;
         boat<<"\\section{Section}"<<std::endl;
         boat<<"Test"<<std::endl;
         boat<<"\\end{document}"<<std::endl;
         boat.close();
-        std::string cmd = "pdflatex "+target;
+        std::string cmd;
+#ifdef __APPLE__
+        cmd+="/usr/local/texlive/2015basic/bin/x86_64-darwin/";
+#endif
+        cmd += "pdflatex "+target;
         system(cmd.c_str());
     }
 };
